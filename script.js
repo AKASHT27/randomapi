a9ad67f183d9e22657639787b21d8c8ea1862a93
AKASHT27 <93002388+AKASHT27@users.noreply.github.com>
2022-01-17 22:45:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,24 @@
-const ACCESS_KEY = "0FNmuTadJOAlYF8_VRD7cqqtiMi7eKw1RZC8hi8fp4U";
-
-const SECRET_KEY = "I2TwyOD5KqbM6r-AxjfEDl-Vbi2FK4x1_kSp9uZIiMk";
-
-const exec = function () {
-  const f = fetch(`https://api.unsplash.com/photos/?client_id=${ACCESS_KEY}`);
-
-  f.then((resp) => {
-    return resp.json();
-  })
-    .then((r) => {
-      console.log(r);
-
-      r.forEach((elem) => {
-        const box = document.getElementById("box");
-        console.log(elem.urls.regular);
-        const img_loc = elem.links.download;
-        const div = document.createElement("div");
-        const img = document.createElement("img");
-        img.src = img_loc;
-        div.appendChild(img);
-        box.appendChild(div);
-      });
+const container = document.querySelector('.container');
+const URL = 'https://dog.ceo/api/breeds/image/random'
+function loadImages(numImages = 10){
+   let i=0;
+    while(i < numImages){
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response=>response.json())
+    .then(data=>{
+    const img =  document.createElement('img');
+    img.src = `${data.message}`
+    container.appendChild(img)
     })
-    .catch((e) => {
-      console.log("Something error in fetvhing");
-    });
-};
+    i++;
+    }   
+    }
 
-exec();
-
-// To load random other images on scrolling to the bottom.
-
-const random_images = function () {
-  var lower = 0;
-  var upper = 100000000;
-
-  for (i = 0; i < 10; i++) {
-    const box = document.getElementById("box");
-    const img_loc =
-      "https://picsum.photos/500/300?random=" +
-      (Math.floor(Math.random() * (upper - lower)) + lower);
-    console.log(img_loc);
-    const div = document.createElement("div");
-    const img = document.createElement("img");
-    img.src = img_loc;
-    div.appendChild(img);
-    box.appendChild(div);
-  }
-};
-
-const body = document.getElementsByTagName("body")[0];
-window.onscroll = function (ev) {
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-    random_images();
-  }
-};
+loadImages();
+window.addEventListener('scroll',()=>{
+    console.log("scrolled", window.scrollY) //scrolled from top
+    console.log(window.innerHeight) //visible part of screen
+    if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+        loadImages();
+    }
+})
